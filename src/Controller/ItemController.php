a52fe15879d4cdb9a8ee7d4ff1e2ffe0ee0cabe1
unome459 <carpenterjameson@gmail.com>
2019-05-22 17:13:18 -0400

    Lab 10 and Final Project

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -9,6 +9,8 @@ use App\Form\ItemType;
 use App\Repository\ItemRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class ItemController extends BaseController {
 
@@ -54,14 +56,95 @@ class ItemController extends BaseController {
     }
 
     /**
-     * @Route("/fridge-list-item", name="item_list_fridge")
+     * @Route("/fridge-list-item", name="item_list_fridge", requirements={"id"="\d+"})
+     * @Method("GET")
      */
     public function list(Request $request) {
         $repository = $this->getDoctrine()->getRepository(ProduceItem::class);
 
-        $items = $repository->getRefrigeratorItems();
+        $items = $repository->findAll();
 
         return $this->render('item-list.html.twig', ['items' => $items]);
     }
 
+    /**
+     * @Route("/item/delete/{id}", name="delete_item", requirements={"id"="\d+"})
+     * @Method("DELETE")
+     */
+    public function deleteItem(int $id) {
+        $repo = $this->getDoctrine()->getRepository(ProduceItem::class);
+        $item = $repo->find($id);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($item);
+        $em->flush();
+
+        return new JsonResponse([], Response::HTTP_NO_CONTENT);
+    }
+    /**
+     * @Route("produce_item/{id}/edit", name="edit_produce_item", requirements={"id"="\d+"})
+     */
+    public function editItem(it $id, Request $request) {
+        $repo = $this->getDoctrine()->getRepository(ProduceItem::class);
+        $item = $repo->find($id);
+
+        $form = $this->createForm(ItemType::class, $item);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($item);
+            $entityManager->flush();
+
+            return new Response('Item ' . $item->getId() . ' got updated!');
+        }
+        return $this->render('new-item.html.twig', ['form' => $form->createView(), 'label' => 'Edit Item']);
+    }
+    /**
+     * @Route("produce_item/{id}", name="ajax_edit_produce_item", requirements={"id"="\d+"})
+     * @Method("PUT")
+     */
+    public function ajaxEditProduceItem(int $id, Request $request) {
+        $item = $this->getDoctrine()->getRepository(ProduceItem::class)->find($id);
+
+        //TODO extract data from Request
+
+        $data = $request->request->all();
+
+        //Save that data to a Item
+
+        $form = $this->createForm(ItemType::class, $item);
+        $form->submit($data);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($item);
+        $entityManager->flush();
+
+        return JsonReponse([], Response::HTTP_OK);
+    }
+    /**
+     * @Route("produce_item/download", name="produce_item_download")
+     */
+    public function download() {
+        $repository = $this->getDoctrine()->getRepository(ProduceItem::class);
+        $items = $repository->findAll();
+        $fileName = 'item_list.txt';
+
+        $fp = fopen($fileName, 'w');
+
+        $content = '';
+
+        foreach($items as $item) {
+            $n = $item->getName();
+            $ed = $item->getExpirtionDate();
+            $i = $item->getImage();
+            $content .= "$n $ed $i:\n";
+
+            fwrite($fp, $content);
+            fclose($fp);
+
+            return $this->file($fileName);
+        }
+    }
 }
